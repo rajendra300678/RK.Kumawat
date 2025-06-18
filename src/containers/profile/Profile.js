22492e54b09c6ec92c97377111f55289a5eac0e5
Rajendra300678 <Rajendra@zrix.com>
2025-06-18 15:50:02 +0530

    jj

--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -1,14 +1,20 @@
-import React, {useState, useEffect, lazy, Suspense} from "react";
-import Contact from "../contact/Contact";
+import React, { useState, lazy, Suspense } from "react";
 import Loading from "../loading/Loading";
 
-const renderLoader = () => <Loading />;
 const GithubProfileCard = lazy(() =>
   import("../../components/githubProfileCard/GithubProfileCard")
 );
+
 export default function Profile() {
   const [prof, setrepo] = useState([]);
+
   function setProfileFunction(array) {
     setrepo(array);
   }
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <GithubProfileCard data={prof} />
+    </Suspense>
+  );
 }
