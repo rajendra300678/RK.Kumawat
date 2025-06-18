4c97cc8a34038a1986abd1ea2a8cb722b972db82
Rajendra300678 <Rajendra@zrix.com>
2025-06-18 18:03:04 +0530

    durga

--- src/portfolio.js
+++ src/portfolio.js
@@ -394,23 +394,15 @@ const achievementSection = {
         }
       ]
     },{
-      title: "Google Code-In Finalist",
+      title: "Navratrii Stickers",
       subtitle:
-        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
-      image: require("./assets/images/codeInLogo.webp"),
-      imageAlt: "Google Code-In Logo",
+        "Elevate your messaging experience with our divine collection of Goddess Durga stickers. Celebrate the strength and grace of the Divine Mother with beautifully crafted stickers for all occasions. Share your devotion and blessings with friends and family.",
+      image: require("./assets/images/Navratrii.webp"),
+      imageAlt: "",
       footerLink: [
         {
-          name: "Certification",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
-        },
-        {
-          name: "Award Letter",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
-        },
-        {
-          name: "Google Code-in Blog",
-          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+          name: "View",
+          url: "https://play.google.com/store/apps/details?id=com.ballapps.durgamatastickers&hl=en"
         }
       ]
     },
