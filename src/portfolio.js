687920d3570f58a2cb71e1397b92016920fad64f
Rajendra300678 <Rajendra@zrix.com>
2025-06-18 17:45:10 +0530

    Holiday

--- src/portfolio.js
+++ src/portfolio.js
@@ -382,23 +382,15 @@ const achievementSection = {
         }
       ]
     },{
-      title: "Google Code-In Finalist",
+      title: "Holiday Greetings Stickers",
       subtitle:
-        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
-      image: require("./assets/images/codeInLogo.webp"),
-      imageAlt: "Google Code-In Logo",
+        "All Holiday Greetings Stickers has the largest collection of stickers that you can share with your friends and family WhatssApp group.",
+      image: require("./assets/images/Holiday.webp"),
+      imageAlt: "",
       footerLink: [
         {
-          name: "Certification",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
-        },
-        {
-          name: "Award Letter",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
-        },
-        {
-          name: "Google Code-in Blog",
-          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+          name: "View",
+          url: "https://play.google.com/store/apps/details?id=com.ballapps.allfstickers&hl=en"
         }
       ]
     },{
